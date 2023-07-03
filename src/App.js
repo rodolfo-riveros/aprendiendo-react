bbd6420f570811f6976efd8b4635a42ff6a21b35
rodolfo-riveros <1001420172@unajma.edu.pe>
2023-07-03 12:06:45 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/pure/greeting';
+import GreetingF from './components/pure/greetingF';
 
 function App() {
   return (
@@ -17,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        {/* Este es mi primer componente */}
+        <Greeting name = { 'Rodolfo' } ></Greeting>
+        <GreetingF name = { 'Rodolfo' }></GreetingF>
       </header>
     </div>
   );
